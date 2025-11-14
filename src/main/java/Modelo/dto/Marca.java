0f0a9ef0c745e14efc226504cab81819d6be28a7
cristobal <cristobal@levova2006>
2025-11-13 19:43:19 -0500

    ....

---
+++ src/main/java/Modelo/dto/Marca.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.dto;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author cristobal
+ */
+public class Marca implements Serializable{
+    private int jidentificacion;
+    private String jmarca;
+
+    public int getJidentificacion() {
+        return jidentificacion;
+    }
+
+    public void setJidentificacion(int jidentificacion) {
+        this.jidentificacion = jidentificacion;
+    }
+
+    public String getJmarca() {
+        return jmarca;
+    }
+
+    public void setJmarca(String jmarca) {
+        this.jmarca = jmarca;
+    }
+    
+
+   
+    
+    
+}
